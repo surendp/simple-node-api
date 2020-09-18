547fd84a9ab43b23037481a0764cf1d2f69817b6
Surendra Pandey <surendra.pandey2@metropolia.fi>
2020-09-18 23:18:40 +0300

    - Create new route to fetch individual todo

--- routes.js
+++ routes.js
@@ -7,4 +7,16 @@ router.get("/todos", function(req, res) {
   res.json(todos);
 });
 
+router.get("/todos/:id", function(req, res) {
+  const id = req.params.id
+  const todo = todos.find(todo => todo.id === Number(id))
+
+  if (todo) {
+    res.json(todo)
+    return
+  }
+
+  res.json({})
+})
+
 module.exports = router;
